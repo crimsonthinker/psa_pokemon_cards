aeab9f9de81d7d47c3eddf3a1b763e54a9d1a420
khoa <khoanguyen0807@gmail.com>
2021-07-28 18:09:53 +0700

    Add data loader

--- utils/loader.py
+++ utils/loader.py
@@ -1,11 +1,12 @@
-import collections
+from matplotlib.image import imread, imsave
 import glob
 import os
 import tensorflow as tf
 import random
-import uuid
+import numpy as np
 import shutil
-from PIL import Image
+import pandas as pd
+from pathlib import Path
 
 from utils.utilities import *
 
@@ -17,77 +18,111 @@ class ImageLoader(object):
         object: python's object type
     """
     def __init__(self, **kwargs):
-        """__init__ function
+        """initialization function for Image Loader. Containing the following parameters
+        train_directory [string] : path to the original training folder
+        grade_path [string] : path to the grade file. Defaults as data/grades.csv
+        skip_preprocessing [bool] : Indicating whether to skip the preprocessing part 
+        batch_size [int] : batch size for the dataset. Defaults as 32.
+        oversampling_ratio_over_max [float] : A sampling ratio between the number of images in a label and the maximum number of images in one label.
+        validation_split [float] : ratio for validation split
+        img_width [float] : image width for the model's input
+        img_height [float] : image height for the model's input
         """
-        self._train_directory = kwargs.get('train_directory', os.path.join('temples-train-hard', 'train'))
-        self._skip_reloading = kwargs.get('skip_reloading', False)
-        self._default_preprocessed_images = '.preprocessed_train_temples'
-        if not self._skip_reloading:
-            if os.path.exists(self._default_preprocessed_images):
-                shutil.rmtree(self._default_preprocessed_images)
-            ensure_dir(self._default_preprocessed_images)
+        self._train_directory = kwargs.get('train_directory', None)
+        self._grade_path = kwargs.get('grade_path', os.path.join('data', 'grades.csv'))
+        self._skip_preprocessing = kwargs.get('skip_reloading', False)
+        self._preprocessed_dataset_path = '.preprocessed_train'
+        if not self._skip_preprocessing:
+            if os.path.exists(self._preprocessed_dataset_path):
+                shutil.rmtree(self._preprocessed_dataset_path)
+            ensure_dir(self._preprocessed_dataset_path)
         self._logger = get_logger("ImageLoader")
 
         self._batch_size = kwargs.get('batch_size', 32)
         self._oversampling_ratio_over_max = kwargs.get('oversampling_ratio_over_max', 0.8)
         self._validation_split = kwargs.get('validation_split', 0.3)
+        self._images = None
+        self._images_file_name = None
+        self._grades = pd.read_csv(self._grade_path, index_col = 'Identifier')
+        self._identifiers = []
+
+
         self.img_width = kwargs.get('img_width')
         self.img_height = kwargs.get('img_height')
-        self.class_names = []
 
     def _split(self):
-        """Split dataset to train
+        """Split dataset into train and test dataset
         """
         # ensure directory
-        ensure_dir(self._default_preprocessed_images)
+        ensure_dir(self._preprocessed_dataset_path)
         #read images
-        images = []
-        for name in glob.glob(os.path.join(self._train_directory, '*', '*')):
-            _ , _ , country,_ = name.split("/")
-            im = Image.open(name).resize((self.img_width, self.img_height))
-            images.append((im, country))
-        random.shuffle(images)
-        num_of_images = len(images)
+        self._images = []
+        self._identifiers = []
+        for name in glob.glob(os.path.join(self._train_directory, '*')):
+            # check if name is a folder
+            if os.path.isdir(name):
+                _, identifier = name.split("/")
+                back_image = os.path.join(name, "back.jpg")
+                front_image = os.path.join(name, "front.jpg")
+                back_image = imread(back_image)
+                front_image = imread(front_image)
+
+                preprocessed_image = self._preprocess(back_image, front_image)
+
+                # append the preprocessed image
+                self._images.append(preprocessed_image)
+                self._identifiers.append(identifier)
+
+        # shuffle using list of indices
+        shuffle_indices = random.shuffe(list(range(len(self._images))))
+
+        # convert list to numpy
+        self._images = np.array(self._images)[shuffle_indices]
+        self._identifiers = [self._identifiers[i] for i in shuffle_indices]
+
+        num_of_images = len(self._images)
         split_thres = int((1.0 - self._validation_split) * num_of_images)
-        self._train_data, self._val_data = images[:split_thres], images[split_thres:]
-        # save validation data to folder
-        for val_image in self._val_data:
-            country = val_image[1]
-            val_image = val_image[0]
-            root_file_path = os.path.join(self._default_preprocessed_images, 'val', country)
-            ensure_dir(root_file_path)
-            val_image.save(os.path.join(root_file_path, f'{str(uuid.uuid4())}.png'))
+        self._train_data, self._val_data = self._images[:split_thres], self._images[split_thres:]
 
-    def _oversample(self):
-        # oversample on train data
-        train_sort = collections.defaultdict(list)
-        for train_image, country in self._train_data:
-            train_sort[country].append(train_image)
-        max_sample_per_class = len(max(train_sort.values(), key = lambda x : len(x)))
+    def _preprocess(self, back_image : np.ndarray, front_image : np.ndarray):
+        """Preprocess image by cropping content out of contour and merge image
 
-        for country in train_sort:
-            for train_image in train_sort[country]:
-                root_file_path = os.path.join(self._default_preprocessed_images, 'train', country)
-                ensure_dir(root_file_path)
-                train_image.save(os.path.join(root_file_path, f'{str(uuid.uuid4())}.png'))
-            if len(train_sort[country]) < int(max_sample_per_class * self._oversampling_ratio_over_max):
-                num_extra_image = int(max_sample_per_class * self._oversampling_ratio_over_max) - len(train_sort)
-                random_images = random.choices(train_sort[country], k = num_extra_image)
-                for random_image in random_images:
-                    root_file_path = os.path.join(self._default_preprocessed_images, 'train', country)
-                    ensure_dir(root_file_path)
-                    random_image.save(os.path.join(root_file_path, f'{str(uuid.uuid4())}.png'))
+        Args:
+            back_image (np.ndarray): [description]
+            front_image (np.ndarray): [description]
+        Return:
+            (np.ndarray) : A preprocessed image
+        """
+        return np.array
 
-    def _preprocess(self):
-        pass
+    def _save(self):
+        """Save data to preprocessed folder
+        """
+        root_train_path = os.path.join(self._preprocessed_dataset_path, 'train')
+        ensure_dir(root_train_path)
+        for i, train_image in enumerate(self._train_data):
+            imsave(train_image, os.path.join(root_train_path,f'{self._identifiers[i]}.jpg'))
+
+        root_val_path = os.path.join(self._preprocessed_dataset_path, 'val')
+        ensure_dir(root_val_path)
+        for i, val_image in enumerate(self._val_data):
+            imsave(val_image, os.path.join(root_val_path,f'{self._identifiers[len(self._train_data) + i]}.jpg'))
                 
-    def _load(self):
+    def _load(self, score_type):
         """Perform loading the images and split into datasets
         """
         autotune = tf.data.AUTOTUNE
 
+        # get labels from train
+        train_path = os.path.join(self._preprocessed_dataset_path, 'train')
+        train_identifier_list = sorted([Path(name).stem for name in glob.glob(train_path)])
+
+        val_path = os.path.join(self._preprocessed_dataset_path, 'val')
+        val_identifier_list = sorted([Path(name).stem for name in glob.glob(val_path)])
+
         self._train_img_ds = tf.keras.preprocessing.image_dataset_from_directory(
-            os.path.join(self._default_preprocessed_images, 'train'),
+            train_path,
+            labels = self._grades.loc[train_identifier_list][score_type],
             label_mode = 'categorical',
             image_size = (self.img_height, self.img_width),
             batch_size = self._batch_size
@@ -96,12 +131,13 @@ class ImageLoader(object):
         self._train_img_ds = self._train_img_ds.cache().shuffle(100).prefetch(buffer_size = autotune)
         
         self._validation_img_ds = tf.keras.preprocessing.image_dataset_from_directory(
-            os.path.join(self._default_preprocessed_images, 'val'),
+            val_path,
+            labels = self._grades.loc[val_identifier_list][score_type],
             label_mode = 'categorical',
             image_size = (self.img_height, self.img_width),
             batch_size = self._batch_size
         )
-        #FIXME: Inconsistent class names
+
         self._validation_img_ds = self._validation_img_ds.cache().prefetch(buffer_size = autotune)
         
 
@@ -121,18 +157,16 @@ class ImageLoader(object):
         """
         return self._validation_img_ds
 
-    def load(self):
+    def load(self, score_type):
         """Load images from directory and split into sets
         """
-        if not self._skip_reloading:
+
+        if not self._skip_preprocessing:
             self._logger.info(f"Perform splitting")
             self._split()
 
-            self._logger.info(f"Oversampling on train data")
-            self._oversample()
-
-            self._logger.info("Preprocess data")
-            self._preprocess()
+            self._logger.info("Save dataset to folder")
+            self._save()
 
         self._logger.info(f"Load images into train and val data")
-        self._load()
+        self._load(score_type)
