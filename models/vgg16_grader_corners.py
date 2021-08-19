73db3793d63a378b2f6f90aac157e4fe63a39f5f
khoa <khoanguyen0807@gmail.com>
2021-08-19 19:18:41 +0700

    Downgrade corner

--- models/vgg16_grader_corners.py
+++ models/vgg16_grader_corners.py
@@ -27,15 +27,7 @@ class VGG16GraderCorners(VGG16GraderBase):
     def _define_meaty_layer(self):
         self._layer_only = tf.keras.Sequential([
             tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(1024, 
-                activation = 'relu',
-                kernel_regularizer = tf.keras.regularizers.l2(0.01)),
-            tf.keras.layers.Dropout(0.3),
-            tf.keras.layers.Dense(1024, 
-                activation = 'relu',
-                kernel_regularizer = tf.keras.regularizers.l2(0.01)),
-            tf.keras.layers.Dropout(0.3),
-            tf.keras.layers.Dense(1024, 
+            tf.keras.layers.Dense(16, 
                 activation = 'relu',
                 kernel_regularizer = tf.keras.regularizers.l2(0.01)),
             tf.keras.layers.Dropout(0.3),
